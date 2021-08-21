6ec528e24c5cd014bffe2f6f05d819c468d5df4c
Mashruf Ehsan <mashruf.ehsan@gmail.com>
2021-08-21 14:09:50 +0600

    Admin login added

--- scripts/loginPage.js
+++ scripts/loginPage.js
@@ -5,6 +5,14 @@ const passWordInput = document.getElementById('exampleInputPassword1');
 let userName, passWord;
 
 loginButton.addEventListener('click',function(){
+
     userName = userNameInput.value;
     passWord = passWordInput.value;
+
+    if(userName == 'admin' && passWord == 'root'){
+        window.location.href = 'logged.html';
+    }
+    else{
+        alert('Invalid username or password.');
+    }
 })
