628370cff5b8b6bc630e07becf29b4c4745bf09f
lami-munira <lamia.munira@northsouth.edu>
2021-08-26 23:30:09 +0600

    oka

---
+++ php/cont.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title> P H P </title>
+  </head>    <body>
+        <div class="container my-3">
+            <h2> More on PHP variables</h2> 
+            <h4> Rules for creating variables in PHP </h4>
+            <ul type = "square">
+            <li> variable starts with Dollar($) sign</li>
+            <li> variable can't start with a numner</li>
+            <li> variable can start with a under score character</li>
+            <li> variable can have aphanumeric character and underscore </li>
+            <li> variable are case sensitive</li>
+
+            </ul>
+         
+</div>
+</body>
+</html>
